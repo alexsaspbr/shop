2244ef59e3b0c6bb3cc444129b5d56359c59dad3
Alex Araujo <alexsaspbr@gmail.com>
2023-09-14 09:14:33 -0300

    criando container docker

--- src/main/java/br/com/ada/reactivejavasw/proxy/ProductProxy.java
+++ src/main/java/br/com/ada/reactivejavasw/proxy/ProductProxy.java
@@ -2,6 +2,7 @@ package br.com.ada.reactivejavasw.proxy;
 
 import br.com.ada.reactivejavasw.dto.ProductDTO;
 import br.com.ada.reactivejavasw.dto.ResponseDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,8 +11,11 @@ import reactor.core.publisher.Mono;
 @Service
 public class ProductProxy {
 
+    @Value("${host.products}")
+    private String hostProducts;
+
     public Mono<ResponseDTO<ProductDTO>> getProductByCode(String code) {
-        return WebClient.create("http://localhost:8080")
+        return WebClient.create(hostProducts)
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/products/{code}")
